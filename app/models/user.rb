c077f84fe1ab9034ca6aa4c42ce413662ff58f9d
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-22 23:50:25 +0100

    create, model, controller and view

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    PASSWORD_REQUIRMENTS = /\A
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case characte
+    /x.freeze
+    validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
+    validates :email, presence: true, uniqueness: true, length: { minimum: 4, maximum: 20 }
+    validates :password, presence: true, length: { minimum: 8, maximum: 20 }, format: { with: PASSWORD_REQUIRMENTS }
+      
+end
