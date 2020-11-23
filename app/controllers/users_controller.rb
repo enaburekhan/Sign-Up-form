3ce1631d6d4bab0ba36200ecab47f1da21a87c65
enaburekhan <ericenaburekhan2018@gmail.com>
2020-11-23 21:01:02 +0100

    refactor codes in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: %i[edit update show]
   def show
-    @user = User.find(params[:id]) 
+    # @user = User.find(params[:id]) 
   end
 
   def new
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id]) 
+    # @user = User.find(params[:id]) 
     if @user.update(user_params)
       flash[:success] = 'user was sucessfully updated'
       redirect_to @user
@@ -29,10 +29,14 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id]) 
+    # @user = User.find(params[:id]) 
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])    
+  end
 
   def user_params
     params.require(:user).permit(:user_name, :email, :password)
